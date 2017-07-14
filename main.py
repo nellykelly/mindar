26a3839578216d816bf28720b9b4b5764b29d4db
TyTerror <tyler.elias.ramsey@gmail.com>
2017-07-14 15:37:31 -0400

    Inported a few libraries and added some psuedocode

--- main.py
+++ main.py
@@ -16,6 +16,10 @@
 #
 import webapp2
 from datetime import datetime
+'''datetime library --->   https://docs.python.org/3/library/datetime.html#datetime.date'''
+from datetime import timedelta
+import time 
+'''time library  --->    https://docs.python.org/2/library/time.html'''
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('')
@@ -28,3 +32,33 @@ class RemHandler(webapp2.RequestHandler):
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
+
+x = datetime.now()
+''' date format = (year, month, day, hour, minutes, seconds, milliseconds)'''
+Event = raw_input("What do you have to do?")
+'''x = datetime.now().replace(microsecond=0)'''
+today = time.localtime().tm_wday
+print today
+'''gives the current day where 0 is Monday and 6 is Sunday'''
+'''
+Current_Time = current time at the moment
+def remind("Event"):
+	ask the question "What do you want to do?"
+	save answer as a variable --> "Event" --> posssibly a dictionary
+	ask "What time?"
+	save the answer as a key of Event
+	ask "Where?"
+	save the answer as a key of Event
+	"Event" = {time, place}'''
+
+
+
+'''BRIEF DESCRIPTION INCOMING
+When website is opened it should check the day and time
+	If its already open then maybe it should refresh every min or so
+When USER clicks REMIMD button it should take them to a form
+	The form ask for the EVENT and the DATE/TIME for the event
+	The EVENT is then saved as a dictionary with the DATE/TIME as keys
+	Maybe these are saved locally?
+If the website is checking and it detects a key with the current date
+	it has an alert or something pop up displaying the EVENT name and time
