03b1ae742125ab834916e8403fe0263871a7db93
nellykelly <niceperson787@gmail.com>
2017-07-14 16:50:29 -0400

    first actual chat try

--- wit.py
+++ wit.py
@@ -6,6 +6,36 @@ def send(request, response):
 def my_action(request):
     print('Received from user...', request['text'])
 
+
+
+
+
+class messages(ndb.Model):
+    content = ndb.StringProperty()
+
+
+class chatHandler(webapp2.RequestHandler):
+	def get(self):
+		content = self.request.get('content')
+
+		content_model = Student(content = content)
+        content_key = content_model.put()
+		print(userms + "user just said this")
+
+
+
+		template = jinja_environment.get_template('chat.html')
+
+		# self.response.out.write(template.render({
+		# 	'' : ,
+		# }))
+
+
+	def post(self):
+
+
+
+
 actions = {
     'send': send,
     'my_action': my_action,
